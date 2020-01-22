0568da2306ffb00ac75e5d6f6504677af2a14a9f
Richard Rishabh <richard.rishabh@ithands.com>
2020-01-22 10:32:22 +0530

    added test.js injected to client vm  using chrome-extesion url

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-console.log('running background.js for the Musician extension and now updated');
+console.log('running background.js for the Musician extension');
 chrome.tabs.query({ url: "*://*.jiosaavn.com/*" }, function callback(tabs) {
 	for (let tab of tabs) {
 		console.log('tab detail : ', tab);
--- inject.js
+++ inject.js
@@ -1,6 +1,7 @@
-console.log('executing injected script from Musician extension on JioSaavn page');
+console.log('Activating Musician extension on JioSaavn page');
+
 var my_awesome_script = document.createElement('script');
 
-my_awesome_script.setAttribute('src', "http://localhost:8181/test.js");
+my_awesome_script.setAttribute('src', "chrome-extension://onhgdoblopmielbooghkdggaccompfeb/test.js");
 
 document.head.appendChild(my_awesome_script);
